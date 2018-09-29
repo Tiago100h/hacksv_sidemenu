585f0c56b39b19809710ee85f2e73f25d04beee2
Tiago Giuliatte Pereira <tiagogiuliatte.p@gmail.com>
2018-09-29 20:10:30 -0300

    Primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { LojasPage } from '../pages/lojas/lojas';
+import { LojaPage } from '../pages/loja/loja';
+import { AlimentacaoPage } from '../pages/alimentacao/alimentacao';
+import { LanchonetePage } from '../pages/lanchonete/lanchonete';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,22 +17,32 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    LojasPage,
+    LojaPage,
+    AlimentacaoPage,
+    LanchonetePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(MyApp)    
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    LojasPage,
+    LojaPage,
+    AlimentacaoPage,
+    LanchonetePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+  ],
+  
 })
+
 export class AppModule {}
